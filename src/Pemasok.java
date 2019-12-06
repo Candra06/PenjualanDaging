82d13b664bd24cd7cd38b027569fc8195d903ba8
fido2001 <54944813+fido2001@users.noreply.github.com>
2019-12-06 13:21:43 +0700

    Tes Pemasok

--- src/Pemasok.java
+++ src/Pemasok.java
@@ -1,2 +1,29 @@
-public class Pemasok {
+import java.util.ArrayList;
+
+public class Pemasok extends Person {
+
+    ArrayList<String> dftrKode = new ArrayList<String>();
+    ArrayList<String> dftrNama = new ArrayList<String>();
+    ArrayList<String> dftrNo_hp = new ArrayList<String>();
+    ArrayList<String> dftrAlamat = new ArrayList<String>();
+
+    public Pemasok(String nama, String no_hp) {
+
+        super(nama, no_hp);
+        dftrKode.add("PM-1");
+        dftrNama.add("Rizal");
+        dftrNo_hp.add("085237689012");
+        dftrAlamat.add("Jalan Karimata No.15");
+    }
+
+    public void showData(){
+        System.out.println("=============== Data Pemasok ===============");
+        for (int i=0; i<dftrKode.size(); i++ ){
+            System.out.println("Kode Pemasok : "+dftrKode.get(i));
+            System.out.println("Nama Pemasok : "+dftrNama.get(i));
+            System.out.println("No HP : "+dftrNo_hp.get(i));
+            System.out.println("Alamat : "+dftrAlamat.get(i));
+            System.out.println("----------------------------------------");
+        }
+    }
 }
