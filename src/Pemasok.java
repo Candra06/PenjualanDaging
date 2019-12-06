c114231b7db0ac6b27956e8d1c44b9e771b0e5b0
fido2001 <54944813+fido2001@users.noreply.github.com>
2019-12-06 13:34:09 +0700

    Tes Pemasok 2

--- src/Pemasok.java
+++ src/Pemasok.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.util.Scanner;
 public class Pemasok extends Person {
 
     ArrayList<String> dftrKode = new ArrayList<String>();
@@ -7,13 +7,43 @@ public class Pemasok extends Person {
     ArrayList<String> dftrNo_hp = new ArrayList<String>();
     ArrayList<String> dftrAlamat = new ArrayList<String>();
 
-    public Pemasok(String nama, String no_hp) {
+    protected String Kode;
 
+    public Pemasok(String nama, String no_hp) {
         super(nama, no_hp);
         dftrKode.add("PM-1");
         dftrNama.add("Rizal");
         dftrNo_hp.add("085237689012");
         dftrAlamat.add("Jalan Karimata No.15");
+    }
+
+    public void addPemasok(){
+        Scanner sc = new Scanner(System.in);
+        boolean dt = false;
+        String res;
+        System.out.println("========== Input Data Pemasok ==========");
+        while (!dt){
+            String lstKode = dftrKode.get(dftrKode.size() - 1);
+            String[] getLst = lstKode.split("-");
+            int get = Integer.parseInt(getLst[1]) + 1;
+            kode = "PM-"+get;
+            System.out.println("Kode User: "+kode);
+            dftrKode.add(kode);
+            System.out.print("Masukkan Nama : ");
+            setNama(sc.nextLine());
+            dftrNama.add(getNama());
+            System.out.print("Masukkan No HP : ");
+            setNo_hp(sc.nextLine());
+            dftrNo_hp.add(getNo_hp());
+            System.out.print("Apakah ingin input lagi? y/n ");
+            res = sc.next();
+            if (res.equals("y")){
+                dt = false;
+            }else {
+                dt = true;
+                showData();
+            }
+        }
     }
 
     public void showData(){
